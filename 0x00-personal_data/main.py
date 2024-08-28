25e8a3b70c70a61a2bb2d60c1a83136184b35090
Medo20300 <medhatconcrete@gmail.com>
2024-08-29 00:10:55 +0300

    loveProgramming

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -1,86 +1,57 @@
 #!/usr/bin/env python3
 """
-Main file
+Main file for handling sensitive data and interacting with the database.
 """
-'''
+
 import logging
-import re
-
-RedactingFormatter = __import__('filtered_logger').RedactingFormatter
-filter_datum = __import__('filtered_logger').filter_datum
-get_db = __import__('filtered_logger').get_db
-hash_password = __import__('encrypt_password').hash_password
-is_valid = __import__('encrypt_password').is_valid
-
-fields = ["password", "date_of_birth"]
-messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
-
-for message in messages:
-    print(filter_datum(fields, 'xxx', message, ';'))
-
-
-message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
-log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
-formatter = RedactingFormatter(fields=("email", "ssn", "password"))
-print(formatter.format(log_record))
-
-get_logger = __import__('filtered_logger').get_logger
-PII_FIELDS = __import__('filtered_logger').PII_FIELDS
-
-print(get_logger.__annotations__.get('return'))
-print("PII_FIELDS: {}".format(len(PII_FIELDS)))
-
-db = get_db()
-cursor = db.cursor()
-cursor.execute("SELECT * FROM users;")
-for row in cursor:
-    print(row[0])
-cursor.close()
-db.close()
-'''
-from encrypt_password import hash_password, is_valid
-
-password = "medhat2030"
-encrypted_password = hash_password(password)
-print(hash_password(password))
-print(hash_password(password))
-print(encrypted_password)
-print(is_valid(encrypted_password, password))
-#!/usr/bin/env python3
-"""
-Main file
-"""
-import logging
-import re
-
 from encrypt_password import hash_password, is_valid
 from filtered_logger import RedactingFormatter, filter_datum, get_db, get_logger, PII_FIELDS
 
-fields = ["password", "date_of_birth"]
-messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
+def main():
+    # Redact sensitive data in messages
+    fields = ["password", "date_of_birth"]
+    messages = [
+        "name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
+        "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"
+    ]
+    
+    print("Redacted Messages:")
+    for message in messages:
+        print(filter_datum(fields, 'xxx', message, ';'))
 
-for message in messages:
-    print(filter_datum(fields, 'xxx', message, ';'))
+    # Format and log a message with sensitive data
+    message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
+    log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
+    formatter = RedactingFormatter(fields=("email", "ssn", "password"))
+    print("\nFormatted Log Record:")
+    print(formatter.format(log_record))
 
-message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
-log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
-formatter = RedactingFormatter(fields=("email", "ssn", "password"))
-print(formatter.format(log_record))
+    # Display logger annotations and PII fields count
+    print("\nLogger Annotations:")
+    print(get_logger.__annotations__.get('return'))
+    print("PII_FIELDS count: {}".format(len(PII_FIELDS)))
 
-print(get_logger.__annotations__.get('return'))
-print("PII_FIELDS: {}".format(len(PII_FIELDS)))
+    # Interact with the database
+    try:
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM users;")
+        
+        print("\nUser Data from Database:")
+        for row in cursor:
+            print(row[0])
+            
+        cursor.close()
+        db.close()
+    except Exception as e:
+        print(f"Database error: {e}")
 
-db = get_db()
-cursor = db.cursor()
-cursor.execute("SELECT * FROM users;")
-for row in cursor:
-    print(row[0])
-cursor.close()
-db.close()
+    # Password encryption and validation
+    password = "MyAmazingPassw0rd"
+    encrypted_password = hash_password(password)
+    print("\nPassword Encryption:")
+    print(f"Encrypted Password: {encrypted_password}")
+    print(f"Is Valid: {is_valid(encrypted_password, password)}")
 
-password = "MyAmazingPassw0rd"
-encrypted_password = hash_password(password)
-print(hash_password(password))
-print(hash_password(password))
-print(encrypted_password)
-print(is_valid(encrypted_password, password))
+if __name__ == "__main__":
+    main()
